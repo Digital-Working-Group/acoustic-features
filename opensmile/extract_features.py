4f6dfc68ed378efd7bbf144ba789750084b15ba1
marisalong <mlong6@bu.edu>
2025-03-06 09:03:35 -0500

    adding support for more featuresets and featurelevels

--- opensmile/extract_features.py
+++ opensmile/extract_features.py
@@ -2,30 +2,28 @@
 run.py
 generate OSM features on WAV files;
 """
+import os
 from osm import extract_osm_features
 
-def extract_llds():
+def extract(audio_fp, **kwargs):
     """
-    extract low level descriptiors
+    extract features from an audio file
+    Inputs: audio_fp: filepath to the audio file
+            feat_level (optional): feature level, default is lld
+            feat_set (optional): feature set, default is ComPareE_2016
     """
-    audio_fp = 'sample_wav/first_ten_Sample_HV_Clip.wav'
-    csv_out = 'sample_out/first_ten_Sample_HV_Clip_lld.csv'
-    extract_osm_features(audio_fp, csv_out, feat_level='lld')
-
-def extract_functionals():
-    """
-    extract functionals
-    """
-    audio_fp = 'sample_wav/first_ten_Sample_HV_Clip.wav'
-    csv_out = 'sample_out/first_ten_Sample_HV_Clip_functionals.csv'
-    extract_osm_features(audio_fp, csv_out, feat_level='functionals')
+    feat_level = kwargs.get('feat_level', 'lld')
+    feat_set = kwargs.get('feat_set', 'ComPareE_2016')
+    basename = os.path.basename(audio_fp).split('.')[0]
+    csv_out = f'sample_out/{basename}_{feat_level}_{feat_set}.csv'
+    extract_osm_features(audio_fp, csv_out, feat_level, feat_set)
 
 def main():
     """
     main entrypoint
     """
-    extract_llds()
-    extract_functionals()
+    extract('sample_wav/first_ten_Sample_HV_Clip.wav')
+    extract('sample_wav/first_ten_Sample_HV_Clip.wav', feat_level='func')
 
 if __name__ == '__main__':
     main()
--- opensmile/osm.py
+++ opensmile/osm.py
@@ -6,22 +6,38 @@ import os
 import opensmile
 import soundfile as sf
 
-def extract_osm_features(audio_fn, csv_out, feat_level='lld'):
+def extract_osm_features(audio_fn, csv_out, feat_level='lld', feat_set='ComPareE_2016'):
     """
     Extracts frequency characteristics of audio using OpenSMILE.
-
+    Default: LowLevelDescriptors and ComPareE_2016
     Writes the features to a CSV
-
     """
-    assert feat_level in {'lld', 'functionals'}, feat_level
-    if feat_level == 'lld':
-        feature_level = opensmile.FeatureLevel.LowLevelDescriptors
-    elif feat_level == 'functionals':
-        feature_level = opensmile.FeatureLevel.Functionals
+    feat_level = feat_level.lower()
+    feat_set = feat_set.lower()
+    assert feat_level in {'lld', 'func', 'lld_de'}, feat_level
+    if feat_level == 'lld_de' and feat_set != 'comparee_2016':
+        print('You selected the feature set LowLevelDescriptors_Deltas, which is only compatible with the feature set ComParE_2016. \
+                  Please fix compatibility and re-run.')
+        return
+    assert feat_set in {'comparee_2016', 
+                        'gemapsv01a', 'gemapsv01b', 
+                        'egemapsv01a', 'egemapsv01b', 'egemapsv02'}, feat_set
+    if feat_set ==  'comparee_2016':
+        feature_set = opensmile.FeatureSet.ComParE_2016
+    elif feat_set == 'gemapsv01a':
+        feature_set = opensmile.FeatureSet.GeMAPSv01a
+    elif feat_set == 'gemapsv01b':
+        feature_set = opensmile.FeatureSet.GeMAPSv01b
+    elif feat_set == 'egemapsv01a':
+        feature_set == opensmile.FeatureSet.eGeMAPSv01a
+    elif feat_set == 'egemapsv01b':
+        feature_set == opensmile.FeatureSet.eGeMAPSv01b
+    elif feat_set == 'egemapsv02':
+        feature_set == opensmile.FeatureSet.eGeMAPSv02
 
     smile = opensmile.Smile(
-        feature_set=opensmile.FeatureSet.ComParE_2016,
-        feature_level=feature_level,
+        feature_set=feature_set,
+        feature_level=feat_level,
     )
 
     audio_samples, audio_sample_rate = sf.read(audio_fn)
