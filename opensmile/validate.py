807196495cf9800c95a2c5ea36b8f6c8d235af06
Cody Karjadi <ckarjadi@fhs.org>
2025-04-04 11:44:45 -0400

    update python3-9-6 requirements, ran pylint on validate.py and applied changes. Notable changes include fixing the reference to hash (inbuilt python func) vs. hash_match (dictitonary) whitespace fixes, unused variables, .items() call

--- opensmile/validate.py
+++ opensmile/validate.py
@@ -58,8 +58,8 @@ def hash_file(file_path):
     Returns the SHA-256 hash of a file
     """
     hasher = hashlib.sha256()
-    with open(file_path, 'rb') as f:
-        for chunk in iter(lambda: f.read(4096), b""):
+    with open(file_path, 'rb') as infile:
+        for chunk in iter(lambda: infile.read(4096), b""):
             hasher.update(chunk)
     return hasher.hexdigest()
 
@@ -70,7 +70,7 @@ def csv_walk(directory_path):
     """
     file_map = {}
 
-    for root, dirs, files in os.walk(directory_path):
+    for root, _, files in os.walk(directory_path):
         for file in files:
             if os.path.splitext(file)[1] != '.csv':
                 continue
@@ -85,13 +85,13 @@ def map_files(test_dir, original_dir):
     # Get all files in both directories
     original_files = csv_walk(original_dir)
     test_files = csv_walk(test_dir)
-    
+
     # Create a mapping of matching files
     matched_files = {}
-    
-    for file_name in test_files:
+
+    for file_name, test_outpath in test_files.items():
         if file_name in original_files:
-            matched_files[file_name] = (test_files[file_name], original_files[file_name])
+            matched_files[file_name] = (test_outpath, original_files[file_name])
     return matched_files
 
 def validate_files(sample_input, python_version):
@@ -102,7 +102,7 @@ def validate_files(sample_input, python_version):
     mapped_dict = map_files('test_output', f'sample_audio/wav/{python_version}')
     summary = [['sample_input', 'original_output', 'test_output', 'original_output_hash',
                  'test_output_hash', 'output_hashes_match', 'cosine_similarity']]
-    
+
     hash_matches = {'match': 0, 'no match': 0}
     for file, (original_output, test_output) in mapped_dict.items():
         try:
@@ -114,22 +114,22 @@ def validate_files(sample_input, python_version):
             if hash_match == 1:
                 hash_matches['match'] += 1
             else:
-                hash['no match'] +=1
-                
+                hash_matches['no match'] +=1
+
             # Cosine similarity
             original_df = pd.read_csv(original_output).drop(['file'], axis=1, errors='ignore')
             test_df = pd.read_csv(test_output).drop(['file'], axis=1, errors='ignore')
             file_no_ext = file.split('.')[0]
-            cosine_similarity = get_cosine_similarity(original_df, test_df, 
-                                                  f'npy/{file_no_ext}.npy', 
+            cosine_similarity = get_cosine_similarity(original_df, test_df,
+                                                  f'npy/{file_no_ext}.npy',
                                                   outdir=f'test_output/{python_version}')
-        
+
             ## Append comparison results
-            summary.append([sample_input, original_output, test_output, 
+            summary.append([sample_input, original_output, test_output,
                                 original_hash, test_hash, hash_match, cosine_similarity])
-        except Exception as e:
-            print(f'Error processing {file}: {e}')
-    
+        except Exception as error:
+            print(f'Error processing {file}: {error}')
+
     outpath = os.path.join('test_output', python_version, f'test_comparison_{python_version}.csv')
     with open(outpath, 'w', newline='') as outfile:
         writer = csv.writer(outfile)
@@ -138,9 +138,7 @@ def validate_files(sample_input, python_version):
     print(f'Validation CSV written to {outpath}.')
 
 if __name__ == '__main__':
-    sample_input = 'sample_audio/wav/first_ten_Sample_HV_Clip.wav'
-    generate_comparison_files(sample_input)
-    validate_files(sample_input, 'python3-13-1')
-    validate_files(sample_input, 'python3-9-6')
-
-
+    sample_filepath = 'sample_audio/wav/first_ten_Sample_HV_Clip.wav'
+    generate_comparison_files(sample_filepath)
+    validate_files(sample_filepath, 'python3-13-1')
+    validate_files(sample_filepath, 'python3-9-6')
