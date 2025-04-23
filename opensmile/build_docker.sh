5db7ae82579eb0ecdd763256b1739d9e1180394c
Cody Karjadi <ckarjadi@bu.edu>
2025-04-23 12:54:14 -0400

    iterate on Dockerfile, build_docker.sh, run_docker.sh

--- opensmile/build_docker.sh
+++ opensmile/build_docker.sh
@@ -3,6 +3,6 @@ if [ $# -eq 1 ];
 then
     docker_name=$1
 else
-    docker_name="osm-feat-rhel8-python3"
+    docker_name="acoustic-feat-ubuntu-python39"
 fi
 docker build -t $docker_name .
--- opensmile/run_docker.sh
+++ opensmile/run_docker.sh
@@ -4,14 +4,14 @@ if [ $# -eq 1 ];
 then
     container_name=$1
 else
-    container_name="osm-feat-rhel8"
+    container_name="acoustic-feat-ubuntu-python39-ctr"
 fi
 
 if [ $# -eq 2 ];
 then
     docker_name=$1
 else
-    docker_name="osm-feat-rhel8-python3"
+    docker_name="acoustic-feat-ubuntu-python39"
 fi
 
 docker run -v $(pwd):/scripts -it --rm --name $container_name $docker_name bash
