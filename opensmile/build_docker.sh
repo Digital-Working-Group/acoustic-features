3e492aa139f181cecb525d77ed3ad4d72a2641c2
Cody Karjadi <ckarjadi@fhs.org>
2025-04-23 16:01:06 -0400

    update container and docker names to reflect the Linux OS

--- opensmile/build_docker.sh
+++ opensmile/build_docker.sh
@@ -3,6 +3,6 @@ if [ $# -eq 1 ];
 then
     docker_name=$1
 else
-    docker_name="acoustic-feat-ubuntu-python39"
+    docker_name="acoustic-feat-debian-python39"
 fi
 docker build -t $docker_name .
--- opensmile/run_docker.sh
+++ opensmile/run_docker.sh
@@ -4,14 +4,14 @@ if [ $# -eq 1 ];
 then
     container_name=$1
 else
-    container_name="acoustic-feat-ubuntu-python39-ctr"
+    container_name="acoustic-feat-debian-python39-ctr"
 fi
 
 if [ $# -eq 2 ];
 then
     docker_name=$1
 else
-    docker_name="acoustic-feat-ubuntu-python39"
+    docker_name="acoustic-feat-debian-python39"
 fi
 
 docker run -v $(pwd):/scripts -it --rm --name $container_name $docker_name bash
